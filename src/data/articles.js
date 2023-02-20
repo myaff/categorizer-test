aa574595b7d34dab00a075592dfa3cbe25252641
myaff <grabka74@gmail.com>
2023-02-20 19:29:50 +0600

    add testing data

---
+++ src/data/articles.js
@@ -0,0 +1,81 @@
+const articles = [
+  {
+    id: 1,
+    title: 'The Benefits of Meditation for Stress Reduction',
+    description:
+      'This post will explore the science behind how meditation can help reduce stress and anxiety, and provide tips for starting a daily meditation practice.',
+    image: 'images/1.png',
+    likes: 0,
+  },
+  {
+    id: 2,
+    title: 'The Top 5 Skills Employers Look for in New Hires',
+    description:
+      'This post will examine the most important skills that employers seek in job candidates and offer strategies for developing those skills.',
+    image: 'images/2.jpg',
+    likes: 1,
+  },
+  {
+    id: 3,
+    title: 'How to Build a Successful E-Commerce Business',
+    description:
+      'This post will provide practical advice for starting and growing an online store, covering topics such as finding a niche, creating a website, and driving traffic to your site.',
+    likes: 3,
+  },
+  {
+    id: 4,
+    title: '10 Healthy Meal Prep Ideas for Busy Professionals',
+    description:
+      'This post will offer simple and nutritious meal prep ideas for people with busy schedules, including recipes and tips for planning ahead.',
+    image: 'images/3.png',
+    likes: 10,
+  },
+  {
+    id: 5,
+    title: 'The Psychology of Procrastination: Why We Delay and How to Overcome It',
+    description:
+      'This post will explore the science behind procrastination and offer strategies for breaking the cycle of delay and getting things done.',
+    likes: 20,
+  },
+  {
+    id: 6,
+    title: 'The Benefits of Traveling Solo: Why Everyone Should Try It Once',
+    description:
+      'This post will highlight the benefits of traveling alone, including increased independence, self-confidence, and personal growth, and offer tips for staying safe and making the most of the experience.',
+    image: 'images/4.png',
+    likes: 99,
+  },
+  {
+    id: 7,
+    title: 'The Future of Artificial Intelligence: Opportunities and Challenges',
+    description:
+      'This post will examine the current state of AI and explore its potential to transform industries and society, as well as the ethical considerations and challenges it presents.',
+    image: 'images/5.jpg',
+    likes: 123,
+  },
+  {
+    id: 8,
+    title: 'How to Build a Strong Personal Brand in the Digital Age',
+    description:
+      'This post will provide tips and strategies for building a personal brand online, including creating a cohesive message, building an audience, and leveraging social media platforms.',
+    image: 'images/6.jpg',
+    likes: 11,
+  },
+  {
+    id: 9,
+    title: 'The Importance of Mindfulness for Mental Health and Well-Being',
+    description:
+      'This post will explore the science behind mindfulness practices and their potential to improve mental health and overall well-being, and offer tips for incorporating mindfulness into daily life.',
+    likes: 66,
+  },
+  {
+    id: 10,
+    title: 'The Top 5 Mistakes New Freelancers Make (And How to Avoid Them)',
+    description:
+      'This post will offer advice for new freelancers on common mistakes to avoid, such as undercharging, poor time management, and failing to network effectively.',
+    image: 'images/7.jpg',
+    likes: 88,
+  },
+];
+//const articles = new Array(10).fill(0).map((_item, i) => ({ title: `Article ${i + 1}`}));
+export default articles;
