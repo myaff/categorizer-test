be13af644594489ebe25c90d1d9395f7c7695fe3
myaff <grabka74@gmail.com>
2023-02-20 20:25:15 +0600

    update config with variable base

--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,33 @@
 /* eslint-disable no-undef */
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import { fileURLToPath, URL } from 'node:url';
 import path from 'node:path';
 import vue from '@vitejs/plugin-vue';
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons';
-
-export default defineConfig({
-  base: '/',
-  plugins: [
-    vue(),
-    createSvgIconsPlugin({
-      iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
-      symbolId: 'icon-[dir]-[name]',
-      inject: 'body-first',
-      customDomId: '__svg__icons__dom__',
-    }),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-  },
-  css: {
-    preprocessorOptions: {
-      styl: {
-        additionalData: `@import '${path.resolve(__dirname, './src/assets/styles/variables')}'`,
+export default defineConfig(({ _command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    base: env.BASE_URL,
+    plugins: [
+      vue(),
+      createSvgIconsPlugin({
+        iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
+        symbolId: 'icon-[dir]-[name]',
+        inject: 'body-first',
+        customDomId: '__svg__icons__dom__',
+      }),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
       },
     },
-  },
+    css: {
+      preprocessorOptions: {
+        styl: {
+          additionalData: `@import '${path.resolve(__dirname, './src/assets/styles/variables')}'`,
+        },
+      },
+    },
+  }
 });
