1ac886cf560bbdbc74c75100a35c7d1f326b8af5
myaff <grabka74@gmail.com>
2023-02-20 19:54:00 +0600

    add useSureModal composable

---
+++ src/composables/useSureModal.js
@@ -0,0 +1,28 @@
+import { reactive, watch, ref } from 'vue';
+
+export default function useSureModal() {
+  const sure = reactive({
+    fn: null,
+    args: [],
+  });
+  const active = ref(false);
+
+  const reset = () => {
+    sure.fn = null;
+    sure.args = [];
+  }
+
+  const checkSure = (fn, ...args) => {
+    sure.fn = fn;
+    sure.args = args;
+    active.value = true;
+  };
+  const handleSure = answer => {
+    if (answer) sure.fn(...sure.args);
+    active.value = false;
+  };
+
+  watch(active, value => { if (!value) reset()});
+
+  return { active, checkSure, handleSure };
+}
